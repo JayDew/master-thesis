5b098ba92a0fa6e48c8970a19f575d37f30441bb
Andrei Geadau <andrei.geadau@yahoo.com>
2022-12-12 16:19:52 +0000

    Updated plot

--- plot.py
+++ plot.py
@@ -1,19 +1,36 @@
 import numpy as np
-from matplotlib import pyplot as plt
-
+import matplotlib.pyplot as plt
 from common import get_params
 
-n, m, N = get_params()
+n, m = get_params()
 
-cvxopt = np.load(f'Temp/cvxopt_{n}_{m}_{N}.npy')
-# qophe = np.load(f'Temp/QOPHE_{n}_{m}_{N}.npy')
-qophe_enc = np.load(f'Temp/QOPHE_encrypted_{n}_{m}_{N}.npy')
-# alexandru = np.load(f'Temp/Alexandru_{n}_{m}_{N}.npy')
+######## creating the TIME plot
+cvxopt = np.load(f'Temp/cvxopt_{n}_{m}_time.npy')
+alexandru = np.load(f'Temp/alexandru_{n}_{m}_time.npy')
+wang = np.load(f'Temp/wang_{n}_{m}_time.npy')
 
-plt.plot(cvxopt, label="optimal solution")
-# plt.plot(qophe, label="unencrypted QOPHE")
-plt.plot(qophe_enc, label="Shoukry et al.")
-# plt.plot(alexandru, label="Alexandru et al.")
-plt.legend(loc="lower right")
-
+data = {'optimum': cvxopt, 'Alexandru et al.': alexandru, 'Wang et al.': wang}
+# creating the bar plot
+plt.bar(list(data.keys()), list(data.values()), color='maroon', width=0.4)
+# convert y-axis to Logarithmic scale
+plt.yscale("log")
+plt.ylabel("Time (seconds) - log scale")
+plt.title(f'Computation time on {n}x{m} matrix')
 plt.show()
+######## end TIME plot
+
+
+######## creating PERFORMANCE plot
+cvxopt = np.load(f'Temp/cvxopt_{n}_{m}.npy')[0][0]
+alexandru = np.load(f'Temp/alexandru_{n}_{m}.npy')
+wang = np.load(f'Temp/wang_{n}_{m}.npy')[0][0]
+
+data = {'optimum': abs(cvxopt), 'Alexandru et al.': abs(alexandru), 'Wang et al.': abs(wang)}
+# creating the bar plot
+plt.bar(list(data.keys()), list(data.values()), color='blue', width=0.4)
+# convert y-axis to Logarithmic scale
+# plt.yscale("log")
+plt.ylabel("Optimum value")
+plt.title(f'Optimal value on {n}x{m} matrix')
+plt.show()
+######## end PERFORMANCE plot
