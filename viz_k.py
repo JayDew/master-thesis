e4df75600eb3f80718bd09159d9bb61e49883c16
Andrei Geadau <andrei.geadau@yahoo.com>
2023-05-31 15:11:05 +0200

    plot accelerated iterations

--- viz_k.py
+++ viz_k.py
@@ -1,50 +1,51 @@
 import matplotlib.pyplot as plt
-from scipy.interpolate import make_interp_spline
-import numpy as np
-import pandas as pd
 
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "sans-serif",
-    "font.sans-serif": "Helvetica",
-})
+# data
+accelerated_2 = [(8, 1), (30, 25), (48, 41), (72, 33), (90, 63), (138, 57), (142, 62), (242, 124), (248, -1),
+                 (354, 141), (376, 191), (438, 80), (536, 382), (624, 248), (724, 736), (772, 135), (884, 154),
+                 (918, 468), (1058, 290), (1154, 515), (1280, 248), (1412, 292), (1546, 314), (1684, 479), (1816, 275),
+                 (1954, 425), (2024, 298), (2284, 368), (2316, 284)]
+accelerated_1_7 = [(8, 1), (30, 93), (48, 236), (72, 168), (90, 386), (138, 351), (142, 223), (242, 415), (248, -1),
+                   (354, 454), (376, -1), (438, 764), (536, -1), (624, 1210), (724, -1), (772, 760), (884, 971),
+                   (918, -1), (1058, -1), (1154, -1), (1280, -1), (1412, -1), (1546, -1), (1684, -1), (1816, -1),
+                   (1954, -1), (2024, -1), (2284, -1), (2316, -1)]
+accelerated_1_5 = [(8, 1), (30, 152), (48, 390), (72, 276), (90, 641), (138, 582), (142, 370), (242, 689), (248, -1),
+                   (354, 754), (376, -1), (438, 1270), (536, -1), (624, -1), (724, -1), (772, 1263), (884, 1613),
+                   (918, -1), (1058, -1), (1154, -1), (1280, -1), (1412, -1), (1546, -1), (1684, -1), (1816, -1),
+                   (1954, -1), (2024, -1), (2284, -1), (2316, -1)]
+accelerated_1 = [(8, 1), (30, 301), (48, 777), (72, 550), (90, 1279), (138, 1162), (142, 739), (242, 1374), (248, -1),
+                 (354, 1505), (376, -1), (438, -1), (536, -1), (624, -1), (724, -1), (772, -1), (884, -1), (918, -1),
+                 (1058, -1), (1154, -1), (1280, -1), (1412, -1), (1546, -1), (1684, -1), (1816, -1), (1954, -1),
+                 (2024, -1), (2284, -1), (2316, -1)]
 
-df = pd.read_csv('res/results.csv')
-# Fill NaN values with column averages
-df = df.fillna(df.mean())
+# parse data
+edges_acc_2 = [x[0] for x in accelerated_2 if x[1] != -1]
+iterations_acc_2 = [x[1] for x in accelerated_2 if x[1] != -1]
+non_conv_acc_2 = [x[0] for x in accelerated_2 if x[1] == -1]
 
-# get the M values
-M = df["M"].unique()
-df = df.set_index('M')
+edges_acc_1_7 = [x[0] for x in accelerated_1_7 if x[1] != -1]
+iterations_acc_1_7 = [x[1] for x in accelerated_1_7 if x[1] != -1]
+non_conv_acc_1_7 = [x[0] for x in accelerated_1_7 if x[1] == -1]
 
-K_avg = np.zeros(len(M))
-K_SD = np.zeros(len(M))
+edges_acc_1_5 = [x[0] for x in accelerated_1_5 if x[1] != -1]
+iterations_acc_1_5 = [x[1] for x in accelerated_1_5 if x[1] != -1]
+non_conv_acc_1_5 = [x[0] for x in accelerated_1_5 if x[1] == -1]
 
-# get the average and standard deviation
-for idx, m in enumerate(M):
-    K = np.array(df.loc[m, 'k'])
-    K_avg[idx] = np.mean(K)
-    K_SD[idx] = np.std(K)
+edges_acc_1 = [x[0] for x in accelerated_1 if x[1] != -1]
+iterations_acc_1 = [x[1] for x in accelerated_1 if x[1] != -1]
+non_conv_acc_1 = [x[0] for x in accelerated_1 if x[1] == -1]
 
-# smooth the curve
-# 300 represents number of points to make between M.min and M.max
-xnew = np.linspace(M.min(), M.max(), 300)
-spl = make_interp_spline(M, K_avg, k=3)
-spl_err = make_interp_spline(M, K_SD, k=3)
-power_smooth = spl(xnew)
-power_smooth_err = spl_err(xnew)
+# plot the lines
+plt.plot(edges_acc_2, iterations_acc_2, linestyle='-', color='C0', label=r'$\beta$ = 2')
+plt.plot(edges_acc_1_7, iterations_acc_1_7, linestyle='-', color='C1', label=r'$\beta$ = 1.7')
+plt.plot(edges_acc_1_5, iterations_acc_1_5, linestyle='-', color='C2', label=r'$\beta$ = 1.5')
+plt.plot(edges_acc_1, iterations_acc_1, linestyle='-', color='C3', label=r'$\beta$ = 1')
 
-# last few points with use a different lambda
-plt.plot(xnew[:-15], power_smooth[:-15], linestyle='-', color='C0', label=r'$\alpha = 0.01$')
-plt.plot(xnew[-15:], power_smooth[-15:], linestyle='--', color='C0', label=r'$\alpha = 0.005$')
-plt.fill_between(xnew, power_smooth - power_smooth_err / 2, power_smooth + power_smooth_err / 2, alpha=0.2,
-                 facecolor='#089FFF', linewidth=4, linestyle='dashdot',
-                 antialiased=True)
-
-plt.legend(loc="upper left", fontsize=15)
+# show the final plot
 ax = plt.subplot(111)
 ax.set_xlabel('Number of edges')
-ax.set_ylabel('Number of iterations for convergence')
-ax.set_title('Average Number of Iterations for Convergence')
+ax.set_ylabel('Iterations required for convergence')
+ax.set_title('')
+ax.legend()
 ax.grid('on')
 plt.show()
